888c0fd12ca5e54b11b610f7f02fae35a2934768
Elodie Meunier <elow.m@live.fr>
2022-05-25 22:25:25 +0200

    [ADD] - First learning project

---
+++ learn-vue-app/src/components/SubscribeComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <table border>
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Age</th>
+        </tr>
+        <tr v-for="(people, index) in lists" :key="index">
+            <td>{{people.firstname}}</td>
+            <td>{{people.lastname}}</td>
+            <td>{{people.age}}</td>
+        </tr>
+    </table>
+</template>
+<script>
+export default {
+    name: 'SubscribeComponent',
+    props: ['lists']
+}
+</script>
+<style>
+    th {
+        background: lightgrey;
+        font-size: 140%;
+    }
+    td {
+        width: 10rem;
+        text-align: center;
+    }
+</style>
---
+++ learn-vue-app/src/router/index.js
@@ -0,0 +1,17 @@
+// import { createRouter, createWebHashHistory } from 'vue-router'
+// import SubscribeComponent from '../component/SubscribeComponent.vue'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: SubscribeComponent
+//   },
+// ]
+
+// const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes
+// })
+
+// export default router
